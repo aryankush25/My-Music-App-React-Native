244aaf7355325eafc450116cc4276145087b5dd4
Aryan Agarwal <aryankush025@gmail.com>
2020-01-12 21:00:04 +0530

    Safe Area view added for iOS

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,14 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
-
-const styles = StyleSheet.create({
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
-
-const FixedDimensionsBasics = () => {
-  return (
-    <View>
-      {/* <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-      <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} /> */}
-      <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
-    </View>
-  );
-};
+import React, {useState} from 'react';
+import {Text, TextInput, View, Button, SafeAreaView} from 'react-native';
+import Touchables from './Touchables';
 
 const App = () => {
-  return <FixedDimensionsBasics />;
+  return (
+    <React.Fragment>
+      <SafeAreaView />
+      <Touchables />
+    </React.Fragment>
+  );
 };
 
 export default App;
