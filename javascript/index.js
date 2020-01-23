2a79973c2079f09c5011cbe2d3d7f62577c2b53e
Aryan Agarwal <aryankush025@gmail.com>
2020-01-24 02:35:16 +0530

    Names and paths changed

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from './store';
-import Navigator from './navigator';
+import Navigator from './navigators';
 
 const Javascript = () => {
   return (
