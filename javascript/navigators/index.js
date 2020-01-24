dd1095f666ba893936a17a33eae7e9c74f003b08
Aryan Agarwal <aryankush025@gmail.com>
2020-01-24 15:34:33 +0530

    Bottom Tab Navigator design updates

--- javascript/navigators/index.js
+++ javascript/navigators/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {Icon} from 'react-native-elements';
 import SignInScreen from './SignInScreen';
 import SignUpScreen from './SignUpScreen';
 
 const AppNavigator = createBottomTabNavigator(
   {
-    SignIn: SignInScreen,
-    SignUp: SignUpScreen,
+    'Sign In': SignInScreen,
+    'Sign Up': SignUpScreen,
   },
   {
-    initialRouteName: 'SignIn',
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused, horizontal, tintColor}) => {
+        const {routeName} = navigation.state;
+        let iconName;
+        let iconColor = focused ? 'white' : 'grey';
+        if (routeName === 'Sign In') {
+          iconName = 'person';
+        } else if (routeName === 'Sign Up') {
+          iconName = 'person-add';
+        }
+        return <Icon name={iconName} color={iconColor} />;
+      },
+    }),
+    tabBarOptions: {
+      inactiveTintColor: 'gray',
+      activeTintColor: 'white',
+      labelStyle: {
+        fontSize: 14,
+      },
+      style: {
+        backgroundColor: '#14192D',
+        paddingTop: 4,
+      },
+    },
   },
 );
 
