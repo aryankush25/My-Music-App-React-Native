9c6dc0aee14faf5676e9749187ce15abda1997aa
aryankush25 <aryankush025@gmail.com>
2020-01-26 20:06:46 +0530

    styled-components used instead StyleSheet

--- javascript/navigators/index.js
+++ javascript/navigators/index.js
@@ -1,17 +1,16 @@
 import React, {useCallback, useEffect} from 'react';
-import {ActivityIndicator, StatusBar, StyleSheet, View} from 'react-native';
+import {ActivityIndicator, View} from 'react-native';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import styled from 'styled-components';
 import {useDefaultAuth} from '../hooks/authHooks';
 import AuthNavigators from './AuthNavigators';
 import AppNavigators from './AppNavigators';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const InitialLoaderContainer = styled(View)`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
 
 const InitialLoadingNavigator = props => {
   const {isLoggedIn} = useDefaultAuth();
@@ -27,10 +26,9 @@ const InitialLoadingNavigator = props => {
   }, [isLoggedIn]);
 
   return (
-    <View style={styles.container}>
+    <InitialLoaderContainer>
       <ActivityIndicator size="large" color="#0000ff" />
-      <StatusBar barStyle="default" />
-    </View>
+    </InitialLoaderContainer>
   );
 };
 
